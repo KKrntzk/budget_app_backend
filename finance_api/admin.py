ea1a1444bcf40bb0d0eb14019985a008903f350e
KKrntzk <sophie.kornetzki@gmail.com>
2026-06-15 11:18:03 +0200

    admin added, urls added

--- finance_api/admin.py
+++ finance_api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Household, HouseholdMember, Income, Category, Budget, SavingsGoal, Transaction
 
-# Register your models here.
+admin.site.register(Household)
+admin.site.register(HouseholdMember)
+admin.site.register(Income)
+admin.site.register(Category)
+admin.site.register(Budget)
+admin.site.register(SavingsGoal)
+admin.site.register(Transaction)
